8b20b200aa2a75cd3301bb9ac543c724cfe7c341
Satyajit Giram <82489539+satyajitgiram@users.noreply.github.com>
2023-04-09 00:33:19 +0530

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ixev+-nd)=)g_turml=tz)w#4@*4pn8v)aut-j0tu4j5_%n4jd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
