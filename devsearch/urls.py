b30cd0fa88a0812e2860be3177ecfd46b99de3cd
satyajitgiram <satyajitgiram2000@gmail.com>
2023-10-22 13:31:27 +0530

    added password reset feature with mail send

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -19,6 +19,7 @@ from app import views
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -26,6 +27,12 @@ urlpatterns = [
     path('',include('users.urls')),
     path('projects/',include('app.urls')),
     path('admin/', admin.site.urls),
+
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name='reset_password'),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'), name='password_reset_complete'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from users import views
 
 urlpatterns = [
-    path('login', views.loginPage, name='login'),
-    path('register', views.registerUser, name='register'),
-    path('logout', views.logoutUser, name='logout'),
-    path('account', views.userAccount, name='account'),
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerUser, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('account/', views.userAccount, name='account'),
     path('', views.profile, name='profile'),
     path('user-profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('edit-account/', views.editAccount, name='edit-account'),
