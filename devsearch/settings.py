0e3d41ee0f570597c5e6ffd3f9f198e5da4bb41e
satyajitgiram <satyajitgiram2000@gmail.com>
2023-11-04 01:03:55 +0530

    added the CORS policy in the app to work with REST api & frontend

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
     'users.apps.UsersConfig',
+    'corsheaders',
     'rest_framework',
 
 ]
@@ -96,6 +97,7 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -196,6 +198,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
